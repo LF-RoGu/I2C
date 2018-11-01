e1c4bd8a1876cd0cddf31e7bb284ea63e8332adb
LProtox <36269176+LProtox@users.noreply.github.com>
2018-10-31 22:15:17 -0600

    Add files via upload

--- I2C.c
+++ I2C.c
@@ -7,13 +7,22 @@
 
 #include "I2C.h"
 
+/*PCR config*/
+static const GPIO_pinControlRegisterType I2C_PCR = GPIO_MUX2|GPIO_PS;
+
 void I2C_init(i2c_channel_t channel, uint32_t system_clock, uint16_t baud_rate)
 {
 	uint32 valueSCL;
-	uint8 mult = 1;
+	/*Set the configuration for I2C_PCR_Tx*/
+	GPIO_pin_control_register(GPIO_B,BIT2,&I2C_PCR);
+	GPIO_data_direction_pin(GPIO_B,GPIO_OUTPUT,BIT2);
+	/*Set the configuration for I2C_PCR_Rx*/
+	GPIO_pin_control_register(GPIO_B,BIT3,&I2C_PCR);
+	GPIO_data_direction_pin(GPIO_B,GPIO_INPUT,BIT3);
 
 	/*I2C baud rate = I2C module clock speed (Hz)/(mul × SCL divider)*/
-	valueSCL = system_clock/(baud_rate*mult);
+	valueSCL = system_clock/(baud_rate*MULT);
+
 	switch(channel)
 	{
 	case I2C_0:
@@ -26,7 +35,7 @@ void I2C_init(i2c_channel_t channel, uint32_t system_clock, uint16_t baud_rate)
 		 * 10 mul = 4
 		 * 11 Reserved
 		 * */
-		I2C0->F = I2C_F_MULT(0);
+		I2C0->F = I2C_F_MULT(2);
 		/*Enable I2C module*/
 		I2C0->C1 = I2C_C1_IICIE_MASK;
 		break;
@@ -40,7 +49,7 @@ void I2C_init(i2c_channel_t channel, uint32_t system_clock, uint16_t baud_rate)
 		 * 10 mul = 4
 		 * 11 Reserved
 		 * */
-		I2C1->F = I2C_F_MULT(0);
+		I2C1->F = I2C_F_MULT(2);
 		/*Enable I2C module*/
 		I2C1->C1 = I2C_C1_IICIE_MASK;
 		break;
@@ -54,7 +63,7 @@ void I2C_init(i2c_channel_t channel, uint32_t system_clock, uint16_t baud_rate)
 		 * 10 mul = 4
 		 * 11 Reserved
 		 * */
-		I2C2->F = I2C_F_MULT(0);
+		I2C2->F = I2C_F_MULT(2);
 		/*Enable I2C module*/
 		I2C2->C1 = I2C_C1_IICIE_MASK;
 		break;
@@ -62,4 +71,23 @@ void I2C_init(i2c_channel_t channel, uint32_t system_clock, uint16_t baud_rate)
 		break;
 	}
 }
-uint8_t I2C_busy();
+uint8_t I2C_busy()
+{
+	/*Verify that the module is not busy so it can continue*/
+	if(I2C0->S && I2C_S_BUSY_MASK)
+		return TRUE;
+	else
+		return FALSE;
+}
+void I2C_mst_or_slv_mode(uint8_t mst_or_slv)
+{
+	if(TRUE == mst_or_slv)
+	{
+		/*Master mode*/
+		I2C0->C1 |= I2C_C1_MST_MASK;
+	}
+	else
+	{
+		I2C0->C1 &= ~(I2C_C1_MST_MASK);
+	}
+}
