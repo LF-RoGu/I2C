20dcffb7cd8d95416325d340b8d4ac1f967d06d6
LProtox <36269176+LProtox@users.noreply.github.com>
2018-11-04 19:12:09 -0600

    MCG

---
+++ MCG.c
@@ -0,0 +1,8 @@
+/*
+ * MCG.c
+ *
+ *  Created on: 04/11/2018
+ *      Author: Luis Fernando
+ */
+
+
---
+++ MCG.h
@@ -0,0 +1,47 @@
+/*
+ * MCG.h
+ *
+ *  Created on: 04/11/2018
+ *      Author: Luis Fernando
+ */
+
+#ifndef MCG_H_
+#define MCG_H_
+
+// Constants for use in pll_init
+#define NO_OSCINIT 0
+#define OSCINIT 1
+
+#define OSC_0 0
+#define OSC_1 1
+
+#define LOW_POWER 0
+#define HIGH_GAIN 1
+
+#define CANNED_OSC  0
+#define CRYSTAL 1
+
+#define PLL_0 0
+#define PLL_1 1
+
+#define PLL_ONLY 0
+#define MCGOUT 1
+
+// MCG Mode defines
+
+#define BLPI 1
+#define FBI  2
+#define FEI  3
+#define FEE  4
+#define FBE  5
+#define BLPE 6
+#define PBE  7
+#define PEE  8
+
+// IRC defines
+#define SLOW_IRC 0
+#define FAST_IRC 1
+
+
+
+#endif /* MCG_H_ */
