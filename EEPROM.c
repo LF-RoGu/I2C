65d7dc0481b1b27b533ab4cc4f507d3c443a93f5
LProtox <36269176+LProtox@users.noreply.github.com>
2018-11-09 00:50:35 -0600

    Add files via upload

--- EEPROM.c
+++ EEPROM.c
@@ -7,52 +7,18 @@
 
 #include "EEPROM.h"
 
-void write_mem(uint16 address,uint8 data)
+static uint8 mem_data[PAGE_SIZE] = {FALSE};
+
+void EEPROM_write_mem(uint16 address,uint8 data)
 {
 	/*Need to separate address to High and Low address*/
 	/*high address*/
 	uint8 Haddr = address >> BIT8;
 	/*low address*/
 	uint8 Laddr =  address;
-	/*Start bit*/
-	I2C_start();
-	/*Send the RTC Address to the register*/
-	I2C_write_byte(EEPROM_WRITE_CONTROL); /*1101/Direccion fisica(A2/A1/A0)*/
-	/*Check if I2C is busy*/
-	I2C_wait();
-	/*Recevie the Acknowledge*/
-	I2C_get_ack();
-	/*imagino un delay aqui tambien*/
 
-	/*Register address*/
-	I2C_write_byte(Haddr); /*Address = data*/
-	/*Check if I2C is busy*/
-	I2C_wait();
-	/*Recevie the Acknowledge*/
-	I2C_get_ack();
-	/*imagino un delay aqui tambien*/
-
-	/*Register address*/
-	I2C_write_byte(Laddr); /*Address = data*/
-	/*Check if I2C is busy*/
-	I2C_wait();
-	/*Recevie the Acknowledge*/
-	I2C_get_ack();
-	/*imagino un delay aqui tambien*/
-
-	/*Send the stop signal*/
-	I2C_stop();
-	/*imagino un delay aqui tambien*/
-}
-
-uint8 read_mem(uint16 address)
-{
-	uint8 data;
-	/*high address*/
-	uint8 Haddr = address >> BIT8;
-	/*low address*/
-	uint8 Laddr = address;
-
+	/*Set as Tx*/
+	I2C_tx_rx_mode(I2C_TX);
 	/*Start bit*/
 	I2C_start();
 	/*Send the RTC Address to the register*/
@@ -61,7 +27,58 @@ uint8 read_mem(uint16 address)
 	I2C_wait();
 	/*Recevie the Acknowledge*/
 	I2C_get_ack();
-	/*imagino un delay aqui tambien*/
+	/*delay*/
+	EEPROM_delay(1000);
+
+	/*Register address*/
+	I2C_write_byte(Haddr); /*Address = data*/
+	/*Check if I2C is busy*/
+	I2C_wait();
+	/*Recevie the Acknowledge*/
+	I2C_get_ack();
+	/*delay*/
+	EEPROM_delay(1000);
+
+	/*Register address*/
+	I2C_write_byte(Laddr); /*Address = data*/
+	/*Check if I2C is busy*/
+	I2C_wait();
+	/*Recevie the Acknowledge*/
+	I2C_get_ack();
+	/*delay*/
+	EEPROM_delay(1000);
+
+	/*Register address*/
+	I2C_write_byte(data); /*Address = data*/
+	/*Check if I2C is busy*/
+	I2C_wait();
+	/*Recevie the Acknowledge*/
+	I2C_get_ack();
+	/*delay*/
+	EEPROM_delay(1000);
+
+	/*Send the stop signal*/
+	I2C_stop();
+}
+
+uint8 EEPROM_read_mem(uint16 address)
+{
+	uint8 data;
+	/*high address*/
+	uint8 Haddr = address >> BIT8;
+	/*low address*/
+	uint8 Laddr = address;
+
+	/*Chance I2C module to Tx mode*/
+	I2C_tx_rx_mode(I2C_TX);
+	/*Start bit*/
+	I2C_start();
+	/*Send the RTC Address to the register*/
+	I2C_write_byte(EEPROM_WRITE_CONTROL); /*1010/Direccion fisica(A2/A1/A0)*/
+	/*Check if I2C is busy*/
+	I2C_wait();
+	/*Recevie the Acknowledge*/
+	I2C_get_ack();
 
 	/*Register address*/
 	I2C_write_byte(Haddr); /*Address = data*/
@@ -77,7 +94,7 @@ uint8 read_mem(uint16 address)
 	I2C_wait();
 	/*Recevie the Acknowledge*/
 	I2C_get_ack();
-	/*imagino un delay aqui tambien*/
+
 
 	/*Send the start bit signal again so we can send now the data to read*/
 	I2C_repeted_start();
@@ -89,9 +106,8 @@ uint8 read_mem(uint16 address)
 	/*Recevie the Acknowledge*/
 	I2C_get_ack();
 	/*imagino un delay aqui tambien*/
-
-	/*Chance I2C module to Rx mode*/
-	I2C_tx_rx_mode(I2C_RX);
+	/*delay*/
+	EEPROM_delay(1500);
 
 	/*Generate ~Acknowledge*/
 	I2C_nack();
@@ -108,4 +124,21 @@ uint8 read_mem(uint16 address)
 	data = I2C_read_byte();
 	/*imagino un delay aqui tambien*/
 	return (data);
+}
+
+void EEPROM_clear_mem()
+{
+	uint16 mem_size;
+	do
+	{
+		mem_data[mem_size] = NULL;
+		mem_size++;
+	}while(mem_size < PAGE_SIZE);
+}
+
+void EEPROM_delay(uint32 delay){
+	uint32 i=delay;
+	while(i!=0){
+		i--;
+	}
 }
--- EEPROM.h
+++ EEPROM.h
@@ -8,13 +8,42 @@
 #ifndef EEPROM_H_
 #define EEPROM_H_
 
-#define WRITE_CONTROL 0xAE
-#define READ_CONTROL 0xAF
+#include "Bits.h"
+#include "I2C.h"
 
+#define EEPROM_WRITE_CONTROL 0xAE
+#define EEPROM_READ_CONTROL 0xAF
+
+#define PAGE_SIZE 0x64
+
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
 /*\brief Funct to write in the address wirtten in the param via I2C
  *\param[add] Address
  *\param[data] data to store
  * */
-void writeMemory(uint16 address,uint8 data);
+void EEPROM_write_mem(uint16 address,uint8 data);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*\brief Funct to write in the address wirtten in the param via I2C
+ *\param[add] Address
+ * */
+uint8 EEPROM_read_mem(uint16 address);
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*\brief Funct to write in the address wirtten in the param via I2C
+ *\param[add] Address
+ * */
+void EEPROM_clear_mem();
+/********************************************************************************************/
+/********************************************************************************************/
+/********************************************************************************************/
+/*\brief Funct to create a delay so we can read the data in the logic analyser
+ *\param[add] delay
+ * */
+void EEPROM_delay(uint32 delay);
 
 #endif /* EEPROM_H_ */
