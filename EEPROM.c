db0f1ac58b53910397171ca5090ff37678a688d9
LProtox <36269176+LProtox@users.noreply.github.com>
2018-11-09 01:43:23 -0600

    Add files via upload

--- EEPROM.c
+++ EEPROM.c
@@ -123,6 +123,7 @@ void EEPROM_write_string_mem(uint16 address,uint8* ptr_data)
 uint8 EEPROM_read_mem(uint16 address)
 {
 	uint8 data;
+	/*Need to separate address to High and Low address*/
 	/*high address*/
 	uint8 Haddr = address >> BIT8;
 	/*low address*/
@@ -188,8 +189,84 @@ uint8 EEPROM_read_mem(uint16 address)
 
 	/*data get the value that's in the I2C_D register*/
 	data = I2C_read_byte();
-	/*imagino un delay aqui tambien*/
+
 	return (data);
+}
+uint8* EEPROM_read_string_mem(uint16 address)
+{
+	uint8 data;
+	/**/
+	uint8 byte_control = FALSE;
+	/*Need to separate address to High and Low address*/
+	/*high address*/
+	uint8 Haddr = address >> BIT8;
+	/*low address*/
+	uint8 Laddr = address;
+
+	/*Change I2C module to Tx mode*/
+	I2C_tx_rx_mode(I2C_TX);
+	/*Start bit*/
+	I2C_start();
+	/*Send the RTC Address to the register*/
+	I2C_write_byte(EEPROM_WRITE_CONTROL); /*1010/Direccion fisica(A2/A1/A0)*/
+	/*Check if I2C is busy*/
+	I2C_wait();
+	/*Recevie the Acknowledge*/
+	I2C_get_ack();
+	/*delay*/
+	EEPROM_delay(EEPROM_DELAY);
+
+	/*Register address*/
+	I2C_write_byte(Haddr); /*Address = data*/
+	/*Check if I2C is busy*/
+	I2C_wait();
+	/*Recevie the Acknowledge*/
+	I2C_get_ack();
+	/*delay*/
+	EEPROM_delay(EEPROM_DELAY);
+
+	/*Register address*/
+	I2C_write_byte(Laddr); /*Address = data*/
+	/*Check if I2C is busy*/
+	I2C_wait();
+	/*Recevie the Acknowledge*/
+	I2C_get_ack();
+	/*delay*/
+	EEPROM_delay(EEPROM_DELAY);
+
+	/*Send the start bit signal again so we can send now the data to read*/
+	I2C_repeted_start();
+
+	/*Send the RTC Address to the register*/
+	I2C_write_byte(EEPROM_READ_CONTROL); /*1010/Direccion fisica(A2/A1/A0)*/
+	/*Check if I2C is busy*/
+	I2C_wait();
+	/*Recevie the Acknowledge*/
+	I2C_get_ack();
+	/*delay*/
+	EEPROM_delay(EEPROM_DELAY);
+
+	/*Change I2C module to Rx mode*/
+	I2C_tx_rx_mode(I2C_RX);
+
+	for(byte_control = FALSE; byte_control < PAGE_SIZE; byte_control++)
+	{
+		/*data get the value that's in the I2C_D register*/
+		data = I2C_read_byte();
+		/*Check if I2C is busy*/
+		I2C_wait();
+		/*Generate ~Acknowledge*/
+		I2C_nack();
+		/*delay*/
+		EEPROM_delay(EEPROM_DELAY);
+
+		/*data get the value that's in the I2C_D register*/
+		data = I2C_read_byte();
+
+		return (data);
+	}
+	/*Send the stop signal*/
+	I2C_stop();
 }
 
 void EEPROM_clear_mem()
